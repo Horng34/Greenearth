1b7fb3bdf000b9056231fcd3a3e3fd1880345c03
Menghorng <horngmeng40@gmail.com>
2021-08-23 23:50:32 +0700

    Second Commit

--- app/src/main/java/com/example/greenearth/Climatechangeactivity.java
+++ app/src/main/java/com/example/greenearth/Climatechangeactivity.java
@@ -6,13 +6,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 
 public class Climatechangeactivity extends AppCompatActivity {
+    String Tipname[]={"Deforestation", "Greenhouse gas emission","Carbon dioxide","Methane","Nitrous oxide","CFCs","Fossil fuels "};
+    int Tipimage[]={R.drawable.deforestationcause,R.drawable.greenhousegas,R.drawable.co,R.drawable.ch4,R.drawable.no2,R.drawable.cfcs,R.drawable.fosiilfuel};
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_climatechangeactivity);
+        listView = (ListView) findViewById(R.id.customListView);
+        CustonBaseAdapter custonBaseAdapter = new CustonBaseAdapter(getApplicationContext(),Tipname,Tipimage);
+        listView.setAdapter(custonBaseAdapter);
     }
 }
+
+
 
--- app/src/main/java/com/example/greenearth/TipActivity.java
+++ app/src/main/java/com/example/greenearth/TipActivity.java
@@ -2,21 +2,43 @@ package com.example.greenearth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class TipActivity extends AppCompatActivity {
+    ImageView imageview1;
+    TextView txtsometip;
     String Tipname[]={"Green your community", "Shop responsibly","Energy efficiency","Stop deforestation","Use renewable energy source","Eat wisely","Plant tree"};
     int Tipimage[]={R.drawable.greencommunity,R.drawable.shop,R.drawable.energy,R.drawable.deforestation,R.drawable.renewable,R.drawable.eatwisely,R.drawable.tree};
-
     ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        imageview1 = findViewById(R.id.imageview1);
+        txtsometip = findViewById(R.id.txtsometip);
         setContentView(R.layout.activity_tip);
        listView = (ListView) findViewById(R.id.customListView);
        CustonBaseAdapter custonBaseAdapter = new CustonBaseAdapter(getApplicationContext(),Tipname,Tipimage);
       listView.setAdapter(custonBaseAdapter);
+      //item click
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String paragraph="Hi";
+                if (position==0)
+                {
+
+                }
+            }
+        });
+
+
     }
 }
